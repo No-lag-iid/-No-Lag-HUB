fa54d57a85dcea27575a1e0ee930680425090c3f
No-Lag-id <patoduck6969@gmail.com>
2025-07-21 11:39:54 +0800

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/wVF7VKBK/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pet-randomizer/Mutation-randomizer/refs/heads/main/Pet-Randomizer-script.txt"))()
